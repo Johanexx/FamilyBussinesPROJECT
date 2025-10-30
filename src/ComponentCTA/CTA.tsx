7828f6d87c3a978607d9cbfdae603750bdd836e6
ponquew <hervindc15@gmail.com>
2025-10-29 23:47:48 -0500

    Avance en CTA y agregar logo y otras vainas raras

--- src/ComponentCTA/CTA.tsx
+++ src/ComponentCTA/CTA.tsx
@@ -1,8 +1,41 @@
-export default function CTA() {
+import React from 'react'
+import "./CTA.css";
+
+export const CTA = () => {
   return (
-    <div>
-      <h1>Página de inicio</h1>
-      <p>Bienvenido a Family Business Project 🚀</p>
+    <div className="cta-container">
+      {/*Lado izquierdo*/}
+      <section className="left">
+        <img src="src/assets/logo.png" alt="Familybusiness Logo" className="logo" />
+
+        <h2 className="headline">
+          Organiza tus ingresos y gastos <br /> de manera simple.
+        </h2>
+
+        <p className="subheadline">
+          Empieza a ahorrar hoy mismo y alcanza tus metas familiares sin estrés.
+        </p>
+
+        <ul className="benefits">
+          <li>✅ 100% gratis y fácil de usar</li>
+          <li>💳 Controla tus finanzas familiares</li>
+          <li>📈 Visualiza tus avances al instante</li>
+          <li>🔒 Tus datos siempre seguros</li>
+        </ul>
+
+        <div className="button-container">
+          <button>Crear mi cuenta gratis</button>
+          <p className="click-text">↑</p>
+          <p className="click-text">Click Aquí</p>
+        </div>
+
+      </section>
+      {/*Lado derecho*/}
+      <section className="right">
+        <div className="banner">
+          <img src="src/assets/dinero.png" alt="Imagen de dinero" className="banner-img" />
+        </div>
+      </section>
     </div>
-  );
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Caracteristicas from './ComponentCaratceristicas/Caracteristicas'
-import CTA from './ComponentCTA/CTA'
 import Home from './ComponentHome/Home'
 import Login from './ComponentLogin/Login'
+import { CTA } from './ComponentCTA/CTA'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
